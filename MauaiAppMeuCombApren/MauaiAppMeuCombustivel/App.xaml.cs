026bc2a990190542d8c30e3b89b6b36ab831e7cd
Arthur <arthursaraiva51@gmail.com>
2025-02-27 17:27:58 -0300

    Adicionar arquivos de projeto.

---
+++ MauaiAppMeuCombApren/MauaiAppMeuCombustivel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauaiAppMeuCombustivel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauaiAppMeuCombApren/MauaiAppMeuCombustivel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauaiAppMeuCombustivel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
